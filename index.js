8886c2f1c6115b478ba5b7162bd118b17f5af09b
Dharavath Gopi <dharavathgopi940@gmail.com>
2019-12-19 13:20:57 +0530

    Modified

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
 
